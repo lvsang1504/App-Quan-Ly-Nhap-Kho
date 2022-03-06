9e2a6da6d745432f0d41e23a80ff6810e0d76af0
admin <luongsang010s@gmail.com>
2022-03-06 16:43:43 +0700

    first commit

---
+++ app/src/main/java/com/nhom2/qly_nhap_kho/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nhom2.qly_nhap_kho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
